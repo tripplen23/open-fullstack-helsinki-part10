9fdb0957dc659c71891ad2e8d13f7e432a0853c9
tripplen63 <tripplen63@gmail.com>
2023-11-17 17:28:22 +0200

    Update 10.10: A platform-specific font

--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,23 @@
+import { Platform } from "react-native";
+
 const theme = {
   colors: {
     textPrimary: "#24292e",
     textSecondary: "#586069",
     primary: "#0366d6",
     appBarBackGroundColor: "#24292e",
-    mainBackground: "#e1e4e8"
+    mainBackground: "#e1e4e8",
   },
   fontSizes: {
     body: 14,
     subheading: 16,
   },
   fonts: {
-    main: "System",
+    main: Platform.select({
+      android: "Roboto",
+      ios: "Arial",
+      default: "System",
+    }),
   },
   fontWeights: {
     normal: "400",
